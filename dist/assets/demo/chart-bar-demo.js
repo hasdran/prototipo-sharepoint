3e3cdc7146ca2edcb9c58c2859b2b7f6058d91ac
Thiago Ephigenio da Costa <thiagoc@cattaliniterminais.com.br>
2020-06-29 16:48:08 -0300

    removida barra lateral

--- dist/assets/demo/chart-bar-demo.js
+++ dist/assets/demo/chart-bar-demo.js
@@ -7,12 +7,12 @@ var ctx = document.getElementById("myBarChart");
 var myLineChart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ["January", "February", "March", "April", "May", "June"],
+    labels: ["Aguardando Escopo", "Aguardando Proposta", "Em Desenvolvimento", "Em Homologação", "Aguardando Versão"],
     datasets: [{
-      label: "Revenue",
+      label: "",
       backgroundColor: "rgba(2,117,216,1)",
       borderColor: "rgba(2,117,216,1)",
-      data: [4215, 5312, 6251, 7841, 9821, 14984],
+      data: [2, 3, 1, 1, 2],
     }],
   },
   options: {
@@ -31,7 +31,7 @@ var myLineChart = new Chart(ctx, {
       yAxes: [{
         ticks: {
           min: 0,
-          max: 15000,
+          max: 10,
           maxTicksLimit: 5
         },
         gridLines: {
